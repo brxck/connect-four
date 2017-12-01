ae329b46fe48b72e320d1432be52bc686e836b53
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-12-01 10:33:11 -0700

    Implement Board#create

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,13 @@
 class Board
+
+  attr_accessor :spaces
+
   def initialize
     @spaces = []
+    create
   end
 
   def create
-    6.times { @space << Array.new(7, 0) }
+    6.times { @spaces << Array.new(7, 0) }
   end
 end
